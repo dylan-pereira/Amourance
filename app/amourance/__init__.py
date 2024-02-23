aa6df423b1e3d4c346ba5ddcdadaf71231ff1953
Samuel Ohayon <samuelohayon0411@gmail.com>
2024-02-23 16:24:41 +0100

    front inpu user

--- app/amourance/__init__.py
+++ app/amourance/__init__.py
@@ -1,17 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
-    def hello_world():
-        return f"<p>Hello, World!</p>"
-
+    def hello(name=None):
+        return render_template('hello.html', name=name)
     return app
-
-
-
-
- 
- # Path: app/amourance/__init__.py
